9eb841c39b1dd538e0503721f39421830c062665
jacob <jacob.koren@zooz.com>
2018-05-28 18:31:47 +0300

    update runtime properties of broker

--- broker/start_DruidBroker.sh
+++ broker/start_DruidBroker.sh
@@ -23,9 +23,9 @@ sed -i "s/druid.port=8082/druid.port=$PORT0/g" $CONF_DIR/runtime.properties
 # Run
 echo "Deploying Broker services ..."
 
-sed -i  "s/-Xms24g/-Xms4g/g" $DRUID_DIR/conf/druid/broker/jvm.config
-sed -i  "s/-Xmx24g/-Xmx4g/g" $DRUID_DIR/conf/druid/broker/jvm.config
-sed -i  "s/druid.processing.numThreads=7/druid.processing.numThreads=4/g" $CONF_DIR/runtime.properties
+sed -i  "s/-Xms24g/-Xms3750m/g" $DRUID_DIR/conf/druid/broker/jvm.config
+sed -i  "s/-Xmx24g/-Xmx3750m/g" $DRUID_DIR/conf/druid/broker/jvm.config
+sed -i  "s/druid.processing.numThreads=7/druid.processing.numThreads=3/g" $CONF_DIR/runtime.properties
 
 
 exec java `cat conf/druid/broker/jvm.config | xargs` -cp conf/druid/_common:conf/druid/broker:lib/* io.druid.cli.Main server broker
